431bfdc1bdc1bcc4fd1b7e6d20a0b47fc4fc4183
abhishekn123 <abhiabhishekn164@gmail.com>
2020-11-04 19:48:00 +0530

    LoginValidationAdded

--- src/app/Modules/authentication/authentication.module.ts
+++ src/app/Modules/authentication/authentication.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
 import { LoginComponent } from './login/login.component';
 import { MaterialModule } from '../material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [ProfileComponent, LoginComponent],
   imports: [
-    CommonModule,MaterialModule
+    CommonModule,MaterialModule,ReactiveFormsModule
   ],
   exports:[LoginComponent,ProfileComponent]
 })
--- src/app/Modules/authentication/login/login.component.ts
+++ src/app/Modules/authentication/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  // Variable Declaration start
+  showPassword:Boolean=false;
+  isLoading:Boolean=false;
+  userCredential=new FormGroup({
+    Email:new FormControl("",[Validators.required]),
+    Password:new FormControl("",[Validators.required])
+  }
+  )
+
+  // Variable Decleration Ends
   constructor() { }
-   showPassword:Boolean=false;
-   isLoading:Boolean=false;
+
   ngOnInit(): void {
   }
   loginUser():void{
 
   }
+
 }
